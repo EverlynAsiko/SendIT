207c0dae4bd563fa33df9d18a85c263568a6cbe6
EverlynAsiko <asikoeverlyn@gmail.com>
2018-11-30 23:54:50 +0300

    Feat: Setup helmet for http headers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const express = require('express')
+const helmet = require('helmet')
 const app = express()
+app.use(helmet())
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
