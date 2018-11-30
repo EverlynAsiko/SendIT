20c66970b4f46d75d302631fcb4372fe900030cc
EverlynAsiko <asikoeverlyn@gmail.com>
2018-12-01 01:28:28 +0300

    Added morgan and celebrate dependencies

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@
 const express = require('express')
 const helmet = require('helmet')
 require('dotenv').config();
+const morgan = require('morgan');
+const { errors } = require('celebrate');
 
 const app = express()
 app.use(helmet())
 const port = 3000
 
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.use(errors())
+
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
+
