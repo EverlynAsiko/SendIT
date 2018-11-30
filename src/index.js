1a987212f2c9113045a4c3fc1ea43f7581e88078
EverlynAsiko <asikoeverlyn@gmail.com>
2018-11-30 23:43:19 +0300

    Feat:Express configured

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello from Node.js');
+'use strict';
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
