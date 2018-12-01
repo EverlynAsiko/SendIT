d1e818515ce4a74a13d2a98b769d22a48530c1aa
EverlynAsiko <asikoeverlyn@gmail.com>
2018-12-01 17:37:31 +0300

    Feat: Added postgresql

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 'use strict';
 
-const express = require('express')
-const helmet = require('helmet')
+const express = require('express');
+const helmet = require('helmet');
 require('dotenv').config();
 const morgan = require('morgan');
 const { errors } = require('celebrate');
+const pg = require('pg');
 
-const app = express()
-app.use(helmet())
-const port = 3000
+const app = express();
+app.use(helmet());
+const port = 3000;
 
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const pool = new pg.Pool({
+	user: 'sysadmin',
+	host: '127.0.0.1',
+	database: 'sendit',
+	password: '123',
+	port: '5432'
+});
 
-app.use(errors())
+pool.query("SELECT NOW()", (err, res) => {
+	console.log(err, res);
+	pool.end();
+});
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use(errors());
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
 
